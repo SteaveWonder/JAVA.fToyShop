2d9286a225639d22234155f3b52a463ec1309d91
Stepan <soulsteff@gmail.com>
2023-08-16 16:48:46 +0300

    addtoy

--- src/Toy.java
+++ src/Toy.java
@@ -1,2 +1,13 @@
+package ToyShop;
+
 public class Toy {
+    int id;
+    String name;
+    int frequency;
+
+    public Toy(int id, String name, int frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
 }
